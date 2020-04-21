37e82cdbd17bcda221a93cefc6c409f7eab34ee7
Easyleases Admin <admin@easyleases.in>
2020-04-21 15:42:40 +0530

    Push

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -75,6 +75,7 @@ class _LoginScreenState extends State<LoginScreen> {
                   try {
                     final user = await _auth.signInWithEmailAndPassword(
                         email: email, password: password);
+                    print(user);
                     if (user != null) {
                       Navigator.pushNamed(context, ChatScreen.id);
                     }
